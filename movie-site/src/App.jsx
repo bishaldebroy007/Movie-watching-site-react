2dca5c07ffcafe668d32efeb4133e052b82920b6
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-01-05 01:41:04 +0600

    initial commit

---
+++ movie-site/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <div>
+        <p>Hello</p>
+      </div>
+      <Text display="How're you?" />
+      <Text display="I hope fine." /> */}
+      
+    </>
+  );
+}
+
+// making a component = learning
+// function Text({ display }) {
+//   return (
+//     <p>{display}</p>
+//   );
+// }
+
+export default App
