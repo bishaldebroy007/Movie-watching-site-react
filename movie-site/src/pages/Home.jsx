f5c8f7cd63d048ff04e73e72e255b19bfd6f834f
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-01-05 03:36:04 +0600

    search box added

--- movie-site/src/pages/Home.jsx
+++ movie-site/src/pages/Home.jsx
@@ -7,8 +7,15 @@ function Home() {
         {id: 3, title: "The Matrix", release: "1998"},
     ]
 
+    const handleSearch = () => {
+
+    }
     return (
         <div className='home'>
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for movies.." className= "search-input" />
+                <button type="submit" className="search-button">Search</button>
+            </form>
             <div className='movies-grid'>
                 {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} /> 
