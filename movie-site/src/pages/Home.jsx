6c7d24846676ad899e6892dd1d05629f1b0bd851
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-01-05 11:04:34 +0600

    search queries added

--- movie-site/src/pages/Home.jsx
+++ movie-site/src/pages/Home.jsx
@@ -1,25 +1,34 @@
 import MovieCard from "../components/moviesCard"
+import {useState} from "react"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+
     const movies = [
         {id: 1, title: "John Wick", release: "2020"},
         {id: 2, title: "Terminator", release: "1999"},
         {id: 3, title: "The Matrix", release: "1998"},
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("------")
     }
     return (
         <div className='home'>
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search for movies.." className= "search-input" />
+                <input type="text" placeholder="Search for movies.." className= "search-input" value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)} />
                 <button type="submit" className="search-button">Search</button>
             </form>
             <div className='movies-grid'>
                 {movies.map((movie) => (
+                    movie.title.toLowerCase().startsWith(searchQuery.toLowerCase()) &&
                     <MovieCard movie={movie} key={movie.id} /> 
-                ))}
+                )
+            )}
 
             </div>
         </div>
