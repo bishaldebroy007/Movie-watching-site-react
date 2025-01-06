093e3e9a96617ba1d3268dd4d20b8d4b8853b43c
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-01-06 13:11:32 +0600

    nav bar

--- movie-site/src/App.jsx
+++ movie-site/src/App.jsx
@@ -1,21 +1,26 @@
 
 import './App.css'
 import Favorites from './pages/Favourites';
+
 import Home from "./pages/Home";
 
 // import MovieCard from './components/moviesCard';
 import {Routes, Route} from "react-router-dom";
+import NavBar from "./components/NavBar";
+
 
 function App() {
 
   return (
+    <div>
+      <NavBar />
     <main className='main-content'>
-
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/favorites' element={<Favorites />} /> 
       </Routes>
     </main>
+    </div>
   );
 }
 
